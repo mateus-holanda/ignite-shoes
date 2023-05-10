6f67f82d4de50dde4f986599d81f026a0523a9e1
Mateus Holanda <mateus23ita@gmail.com>
2023-05-10 00:37:51 -0300

    Refactor Notification component

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
+import { useEffect } from 'react';
 import { StatusBar } from 'react-native';
-import OneSignal from 'react-native-onesignal';
+import OneSignal  from 'react-native-onesignal';
 import { NativeBaseProvider } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
@@ -13,10 +14,30 @@ import { CartContextProvider } from './src/contexts/CartContext';
 
 OneSignal.setAppId('accc805b-b3ae-48a8-b823-ec2495dd1350');
 
+OneSignal.promptForPushNotificationsWithUserResponse();
+
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   tagUserInfoCreate();
+
+  useEffect(() => {
+    const unsubscribe = OneSignal.setNotificationOpenedHandler((response) => {
+
+      const { actionId } = response.action as any;
+
+      switch (actionId) {
+        case '1':
+          console.log('Option 1');
+        case '2':
+          console.log('Option 2');
+        default:
+          console.log('No action buttons clicked.')
+      };
+    });
+
+    return () => unsubscribe;
+  }, [])
 
   return (
     <NativeBaseProvider theme={THEME}>
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,17 +1,42 @@
+import { useEffect, useState } from 'react';
 import { useTheme } from 'native-base';
 import { DefaultTheme, NavigationContainer } from '@react-navigation/native';
+import OneSignal, { NotificationReceivedEvent, OSNotification } from 'react-native-onesignal';
 
 import { AppRoutes } from './app.routes';
 
+import { Notification } from '../components/Notification';
+
 export function Routes() {
+  const [notification, setNotification] = useState<OSNotification>();
+
   const { colors } = useTheme();
 
   const theme = DefaultTheme;
   theme.colors.background = colors.gray[700];
 
+  useEffect(() => {
+    const unsubscribe = OneSignal.setNotificationWillShowInForegroundHandler(
+      (notificationReceived: NotificationReceivedEvent) => {
+        const response = notificationReceived.getNotification();
+
+        setNotification(response);
+    });
+
+    return () => unsubscribe;
+  }, []);
+
   return (
     <NavigationContainer theme={theme}>
       <AppRoutes />
+
+      {
+        notification?.title &&
+        <Notification
+          data={notification}
+          onClose={() => setNotification(undefined)}
+        />
+      }
     </NavigationContainer>
   );
 }
